99c46096ffbea481a3ccdfff4ef2e25ddb7e324f
Tom van Swam <tom@vanswam.com>
2020-08-17 11:01:19 +0000

    Direction abbreviations in local language, or forced by config #22

--- src/const.ts
+++ src/const.ts
@@ -22,7 +22,24 @@ export const COMPASS_POINTS = {
   NW: 315,
   NNW: 337.5,
 };
-export const COMPASS_ABBREVIATIONS = Object.keys(COMPASS_POINTS);
+export const COMPASS_ABBREVIATIONS = [
+  localize('directions.N'),
+  localize('directions.NNE'),
+  localize('directions.NE'),
+  localize('directions.ENE'),
+  localize('directions.E'),
+  localize('directions.ESE'),
+  localize('directions.SE'),
+  localize('directions.SSE'),
+  localize('directions.S'),
+  localize('directions.SSW'),
+  localize('directions.SW'),
+  localize('directions.WSW'),
+  localize('directions.W'),
+  localize('directions.WNW'),
+  localize('directions.NW'),
+  localize('directions.NNW'),
+];
 export const UNAVAILABLE = localize('common.invalid');
 
 export const INDICATORS = ['arrow_outward', 'arrow_inward', 'circle'];
@@ -34,4 +51,5 @@ export const CONFIG_INDICATOR = 'indicator';
 export const CONFIG_DIRECTION_OFFSET = 'direction_offset';
 export const CONFIG_NAME = 'name';
 export const CONFIG_SHOW_NORTH = 'show_north';
+export const CONFIG_LANGUAGE = 'language';
 export const CONFIG_DOMAINS = ['sensor', 'input_number', 'input_text'];
