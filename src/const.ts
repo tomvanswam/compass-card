a63a5f1fe451b76b8598dc4f81e9bd6c879cac24
Tom van Swam <tom@vanswam.com>
2020-11-27 20:04:44 +0100

    Bump version

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 import { localize } from './localize/localize';
 
-export const CARD_VERSION = '1.0.0-dev';
+export const CARD_VERSION = '1.0.0';
 export const ICONS = {
   compass: 'mdi:compass',
 };
